9d3f798d3f1c2aa2b118981f22b2c1f149b22dbe
Mark Rivera <Mriver1991@gmail.com>
2020-05-18 00:01:15 -0400

    finished sprint

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,28 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    const { getByText } = render(<CheckoutForm />);
 
-test("form shows success message on submit with form details", () => {});
+    const formHeader = getByText(/Checkout Form/i);
+    expect(formHeader).toBeInTheDocument()
+    expect(formHeader).toBeVisible()
+    expect(formHeader.textContent).toMatch(/Checkout Form/i);
+});
+
+test("form shows success message on submit with form details", async () => {
+    // Render
+    const { findByTestId, getByText } = render(<CheckoutForm />);
+
+    // Click checkout button
+    const submitButton = getByText("Checkout");
+    fireEvent.click(submitButton);
+
+
+    const successMessage = await findByTestId("successMessage");
+    expect(successMessage).toBeInTheDocument();
+    expect(successMessage).toBeVisible();
+});
